5247a98a0fe87773fef742a627d052f6aada1611
Snehal <Snehal@LAPTOP-7VQQRKDB>
2024-01-23 10:39:58 +0530

    sample program committed

---
+++ src/test/java/PracticeCommitPush/sampleProgram.java
@@ -0,0 +1,11 @@
+package PracticeCommitPush;
+
+public class sampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+	}
+
+}
