89c775bae9fd675b17c8b0c5d99a42b03878486a
Snehal <Snehal@LAPTOP-7VQQRKDB>
2024-01-23 11:00:27 +0530

    second try attempt

---
+++ src/test/java/PracticeCommitPush/Practice02.java
@@ -0,0 +1,11 @@
+package PracticeCommitPush;
+
+public class Practice02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is second try to commit and push");
+
+	}
+
+}
